7578270da676ddfe91d34b89414e9d6b90fe81a4
nikki kiga <42276368+nikki-kiga@users.noreply.github.com>
2021-07-05 16:42:45 -0700

    remove redux

---
+++ src/gameService.ts
@@ -0,0 +1,14 @@
+import { Observable } from './observable';
+
+
+export interface Cell {
+  mode: -1 | 0 | 1 | 2; //dead < 0 and born is 2
+}
+
+export class GameService {
+  //start a board
+  //start / stop game
+  //randomize board
+  //clear board
+
+};
